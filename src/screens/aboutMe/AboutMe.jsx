27acd5c3406fe3d874e3bd700f50b4d3c0a22417
Shakib40 <shakibjilani@gmail.com>
2022-02-01 13:03:03 +0530

    completed

--- src/screens/aboutMe/AboutMe.jsx
+++ src/screens/aboutMe/AboutMe.jsx
@@ -5,13 +5,14 @@ import person from '../../images/photo.png'
 function AboutMe() {
   return (
     <div className="about__container" id="about-me">
-     <div>
-      <img src={person} alt="person icon"/>
-     </div>
-     <div className="about__text">
-       <h1>ABOUT ME</h1>
-       <p>A passionate Full Web Developer having experience of building web application with JavaScript/Reactjs/Nodejs/ Reduxjs and some other cool libraries and frameworks.</p>
-     </div>
+      <div>
+        <img src={person} alt="person icon" />
+      </div>
+      <div className="about__text">
+        <h1>ABOUT ME</h1>
+        <p>A passionate Full Web Developer having experience of building web application with JavaScript/Reactjs/Nodejs/ Reduxjs and some other cool libraries and frameworks.I'm still enthusiastically learning more programming languages, frameworks, or principles I can integrate into the coding web in my head.
+          Besides coding, I love traveling and meeting new people, playing Cricket or any online game.</p>
+      </div>
     </div>
   )
 }
