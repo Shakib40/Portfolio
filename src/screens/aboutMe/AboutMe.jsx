207697b6e1f618739a99b7da60b0e72e3a348a1e
Shakib40 <shakibjilani@gmail.com>
2022-02-01 15:09:30 +0530

    commit

--- src/screens/aboutMe/AboutMe.jsx
+++ src/screens/aboutMe/AboutMe.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import './aboutMe.css'
 import person from '../../images/photo.png'
+import { saveAs } from "file-saver";
 
 function AboutMe() {
+
+  const saveFile = () => {
+    saveAs(
+      "https://drive.google.com/uc?export=download&id=1UyvoFNFrsCRIUka9n8wkcPCW4wc_a3XF",
+      "resume.pdf"
+    );
+  };
+
+
   return (
     <div className="about__container" id="about-me">
       <div>
@@ -12,6 +22,7 @@ function AboutMe() {
         <h1>ABOUT ME</h1>
         <p>A passionate Full Web Developer having experience of building web application with JavaScript/Reactjs/Nodejs/ Reduxjs and some other cool libraries and frameworks.I'm still enthusiastically learning more programming languages, frameworks, or principles I can integrate into the coding web in my head.
           Besides coding, I love traveling and meeting new people, playing Cricket or any online game.</p>
+        <button onClick={saveFile} className="btn-resume">Download Resume</button>
       </div>
     </div>
   )
