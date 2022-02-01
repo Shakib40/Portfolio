1355f5b0dc44d1c889352342f411ed58329d76b3
Shakib40 <shakibjilani@gmail.com>
2022-02-01 10:45:01 +0530

    ADDED PROJECT

--- src/helpers/portfolioData.js
+++ src/helpers/portfolioData.js
@@ -1,129 +1,34 @@
 //add project on top for latest
-import chat from '../images/projectsicon/9chat.gif'
-import touringNat from '../images/projectsicon/touringnat.gif';
-import portfoliov1 from "../images/projectsicon/portfoliov1.JPG";
-import map from "../images/projectsicon/map.png";
-import tindog from "../images/projectsicon/tindog.png";
-import rememberSeq from "../images/projectsicon/remember-seq.png";
-import drumkit from "../images/projectsicon/drumkit.png";
-import interactiveparticles from "../images/projectsicon/interactiveparticles.gif";
-import generativeArt from "../images/projectsicon/generative_art.png";
-import gorjeo from "../images/projectsicon/gorjeo3.jpg";
-import spartikus from "../images/projectsicon/spartikus2.jpg";
-import stooltracker from "../images/projectsicon/stooltracker.jpg";
-import weatherteller from "../images/projectsicon/weatherteller.jpg";
-import todos from "../images/projectsicon/todos.png";
-import calc from "../images/projectsicon/calc.png";
+import Nordstrom from '../images/projectsicon/Nordstrom.gif';
+import Internshala from '../images/projectsicon/Internshala.gif';
+import Reliance from '../images/projectsicon/Reliance.gif';
+
 
 //add project on top for latest
 
 const portfolioData = [
   {
-    image:  chat,
-    link: 'https://chat-app-juancamacho.netlify.app/',
-    desc: '9 Chat',
-    summary: 'An app to chat using Firebase as a databse',
-    tech: 'React'
+    image:  Nordstrom,
+    link: '',
+    desc: 'We were a team of 5 from Masai Web-12 batch worked on cloning of Nordstrom webpage as part of our Fifth unit construct week. Our project demonstrates a basic flow of and E-Commerce page were users can purchase products.',
+    summary: 'Nordstrom, Inc. is an American luxury department store chain. Founded in 1901 by John W. Nordstrom and Carl F. Wallin, it originated as a shoe store and evolved into a full-line retailer with departments for clothing, footwear, handbags, jewelry, accessories, cosmetics, and fragrances.',
+    tech: 'REACT | NODE-JS | EXPRESS-JS | MONGODB | NPM PACKAGES | MATERIAL UI'
   },
   {
-    image:  touringNat,
-    link: 'https://touringnat.netlify.app/',
-    desc: 'Touring Nat',
-    summary: 'The powers of CSS',
-    tech: 'HTML | CSS | SASS'
+    image:  Internshala,
+    link: 'http://ec2-35-154-40-77.ap-south-1.compute.amazonaws.com/',
+    desc: 'Internshala is an internship and online training platform, based in Gurgaon, India',
+    summary: 'We, as a team of four members, distributed our parts from this website, and each individual was assigned with their specific task and the project was made by mutually collaborating via git, and the development files were stored as repositories on github.com.',
+    tech: 'HTML | CSS | JAVASCRIPT  | MONGODB | MONGOOSE | EJS | NODE-JS | EXPRESS-JS | MONGODB-ATLAS'
   },
+
   {
-    image:  portfoliov1,
-    link: 'https://juancamachoportfolio-first.netlify.app/',
-    desc: 'First Version of my portfolio',
-    summary: 'When I graduated from General Assembly School',
-    tech: 'HTML | CSS | JavaScript | Bootstrap'
+    image:  Reliance,
+    link: 'https://relianceretailclone.netlify.app/',
+    desc: 'A clone of Reliance Retail website. It is an informative site where user can get information about various brands of Reliance.',
+    summary: 'Reliance Retail is the retail initiative of Reliance Industries Limited and is central to the group’s consumer facing businesses. Reliance Retail has been at the forefront of bringing about Organized Retail revolution in India',
+    tech: 'HTML | CSS | JAVASCRIPT |'
   },
-  {
-    image:  map,
-    link: 'https://jcgooglemapsreplica.netlify.app/',
-    desc: 'Google map clone project',
-    summary: 'This app recreates a functional GPS system',
-    tech: 'JavaScript | CSS | HTML'
-  },
-  {
-    image:  tindog,
-    link: 'https://tindogclone.netlify.app/',
-    desc: 'Tindog clone project',
-    summary: 'Tinder for dogs',
-    tech: 'HTML | CSS | JavaScript | Bootstrap'
-  },
-  {
-    image:  rememberSeq,
-    link: 'https://rememberthecolor.netlify.app/',
-    desc: 'Remember the sequence game',
-    summary: 'remember the sequence of the colors, with sounds too',
-    tech: 'HTML | CSS | JavaScript'
-  },
-  {
-    image:  drumkit,
-    link: 'https://browserdrumkit.netlify.app/',
-    desc: 'Drumkit game',
-    summary:'make some beats!',
-    tech: 'HTML | CSS | JavaScript'
-  },
-  {
-    image:  interactiveparticles,
-    link: 'https://interactiveparticles.netlify.app/',
-    desc: 'Interactive particles',
-    summary: 'this is just a cool effect when hovering with your mouse',
-    tech: 'HTML | CSS | JavaScript'
-  },
-  {
-    image:  generativeArt,
-    link: 'https://generativeart.netlify.app/',
-    desc: 'Generative Art project',
-    summary: 'click and drag to draw whatever you want',
-    tech: 'HTML | CSS | JavaScript'
-  },
-  {
-    image:  gorjeo,
-    link: 'https://gorjeo.netlify.app/',
-    desc: 'Gorjeo project',
-    summary: 'last project for the bootcamp at General Assembly',
-    tech: 'React | Ruby | Rails | CSS | PostgreSQL'
-  },
-  {
-    image:  spartikus,
-    link: 'https://sportikus.netlify.app/',
-    desc: 'Spartikus project',
-    summary: 'third project for the bootcamp at General Assembly',
-    tech: 'React | Mongodb'
-  },
-  {
-    image:  stooltracker,
-    link: 'https://stooltracker.netlify.app/',
-    desc: 'Stool Tracker project',
-    summary: 'second project for the bootcamp at General Assembly',
-    tech: 'React | Airtable DB'
-  },
-  {
-    image:  weatherteller,
-    link: 'https://theweatherteller.netlify.app/',
-    desc: 'Weather Teller project',
-    summary: 'my very first project for the bootcamp at General Assembly',
-    tech: 'HTML | CSS | JavaScript'
-  },
-  {
-    image:  todos,
-    link: 'https://juancamachotodoapp.netlify.app/',
-    desc: 'Todos project',
-    summary: 'second side project to train skills',
-    tech: 'HTML | CSS | JavaScript'
-  },
-  {
-    image:  calc,
-    link: 'https://calculator-jc.netlify.app/',
-    desc: 'Calculator project',
-    summary: 'first side project to train my skills',
-    tech: 'HTML | CSS | JavaScript'
-  },
-  
 ]
   
 export default portfolioData;
